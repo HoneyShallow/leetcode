5d7c1d4e203f7521b863c44c64e0699f3cb7dceb
rookie <daicong156@gmail.com>
2021-09-01 15:14:00 +0800

    modify 比较版本

--- 09-01_compare_versions.cpp
+++ 09-01_compare_versions.cpp
@@ -7,13 +7,17 @@ int compare_version(string version1, string version2){
 	int p1 = 0, p2 = 0;
 	while(p1 < m || p2 < n){
 		int x = 0;
+		// x为version1 中.前的有效整数
 		for(; p1 < m && version1[p1] != '.'; p1++)
 			x = x*10 + version1[p1]-'0';
-		p1++;
+		p1++; // 跳过.
+		// y为 version2 中.前的有效整数
 		int y = 0;
 		for(; p2 < n && version2[p2] != '.'; p2++)
 			y = y*10 + version2[p2]-'0';
 		p2++;
+		// 逐次比较x与y的值也即比较两个版本号，若不等，则可以直接返回大小
+		// 若等，比较下一个.前的整数，直至遍历完某个版本号
 		if(x != y)
 			return x > y?1:-1;
 	}
