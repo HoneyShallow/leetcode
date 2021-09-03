987e388e2335388ef33ce21a3d80c23ef1bb60ff
rookie <daicong156@gmail.com>
2021-09-03 08:32:35 +0800

    modify

--- 双指针/09-02-reverse_k_node.cpp
+++ 双指针/09-02-reverse_k_node.cpp
@@ -1,35 +1,39 @@
 #include <iostream>
+#include <vector>
+using namespace std;
 
 struct ListNode {
 public:
 	int val;
 	ListNode * next;
 	ListNode(int x):val(x), next(nullptr){}
-}
+};
 
-ListNode* init_list(vector<int> elems){
+ListNode* init_list(vector<int>& elems){
 	int num = elems.size(); 
 	if(num == 0) return nullptr;
 	/*采用尾插法*/
-	ListNode * tail = nullptr, *head = nullptr;
-	head->next = tail;
+       	ListNode * head = new ListNode(-1);
+	ListNode * tail = head;
 	for(auto & elem:elems){
-		ListNode* tmp = new listNode(elem);
+		ListNode* tmp = new ListNode(elem);
 		tail->next = tmp;
 		tail = tmp;
 	}
 	return head->next;
 }
 void print_list(ListNode * head){
-	listNode * tmp = head->next;
+	ListNode * tmp = head;
 	while(tmp != nullptr){
 		std::cout << tmp->val << " "; 
+		tmp = tmp->next;
 	}
 	std::cout << std::endl;
 }
 ListNode* getKthFromEnd(ListNode* head, int k){
 	//方案1：首先顺序遍历一遍链表共有n个结点
 	// 倒数第k个结点，也即正数第n-k个结点
+	/*
 	int num = 0;
 	ListNode * tmp = head;
 	while(tmp != nullptr){
@@ -43,6 +47,7 @@ ListNode* getKthFromEnd(ListNode* head, int k){
 		dir_in--;
 	}
 	return tmp;
+	*/
 	// 方案2：快指针先请进k步，然后快慢指针在同时一步前进，
 	// 当快指针到达超尾结点时，慢结点正好指向倒数第k个结点
 	ListNode* fast = head, *slow = head;
@@ -61,5 +66,6 @@ int main(){
 	ListNode * head = init_list(data);
 	ListNode * kth_node = getKthFromEnd(head, k);
 	print_list(kth_node);
+	//print_list(head);
 	return 0;
 }
